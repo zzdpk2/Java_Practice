994c14f2837cc4a0575c165b4d06047b1ba6f84a
zhu00100 <zhu00100@umn.edu>
2019-06-11 05:07:12 -0700

    update

---
+++ com.Itcast/StaticChunk/src/StaticChunkPractice.java
@@ -0,0 +1,14 @@
+public class StaticChunkPractice {
+//    static{
+//        System.out.println("Executed!");
+//    }
+//
+//    public StaticChunkPractice() {
+//        System.out.println("Constructor!");
+//    }
+
+    public static void main(String[] args) {
+        StaticChunkPractice staticChunkPractice_1 = new StaticChunkPractice();
+        StaticChunkPractice staticChunkPractice_2 = new StaticChunkPractice();
+    }
+}
