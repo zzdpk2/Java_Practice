55000338677e2b37980a13fd0faec4cf431f14d4
zhu00100 <zhu00100@umn.edu>
2019-06-11 00:31:34 -0700

    update

--- com.Itcast/ArrayListReturn/src/ArrayListReturnDemo.java
+++ com.Itcast/ArrayListReturn/src/ArrayListReturnDemo.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ArrayListReturnDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = build();
+        System.out.println(arrayList);
+        System.out.println(Restructure(arrayList));
+    }
+
+    public static ArrayList<Integer> build(){
+        final int COUNT = 20;
+        ArrayList<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        int num = 0;
+        for (int i = 0; i < COUNT; i++) {
+            num = random.nextInt(101);
+            list.add(num);
+        }
+        return list;
+    }
+
+    public static ArrayList<Integer> Restructure(ArrayList<Integer> list){
+        ArrayList<Integer> evenlist = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i) % 2 == 0)
+                evenlist.add(list.get(i));
+        }
+        return evenlist;
+    }
 }
