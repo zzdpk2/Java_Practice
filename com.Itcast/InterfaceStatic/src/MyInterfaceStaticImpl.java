2ea785bd8d6bcc67a8cbf026591f542a8f05a169
zhu00100 <zhu00100@umn.edu>
2019-06-14 03:41:48 -0700

    update

---
+++ com.Itcast/InterfaceStatic/src/MyInterfaceStaticImpl.java
@@ -0,0 +1,5 @@
+public class MyInterfaceStaticImpl {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ com.Itcast/InterfaceStatic/src/MyStaticInterface.java
@@ -0,0 +1,5 @@
+public interface MyStaticInterface {
+    public static void methodStatic(){
+        System.out.println("This is a static method!");
+    }
+}
