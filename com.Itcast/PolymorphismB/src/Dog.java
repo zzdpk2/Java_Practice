dab2edaaf2bf7c1c3c4f890470f4c9a7c1526f02
zhu00100 <zhu00100@umn.edu>
2019-06-21 14:52:33 -0700

    new

---
+++ com.Itcast/PolymorphismB/src/Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends Animal{
+    @Override
+    public void eat() {
+        System.out.println("Shit");
+    }
+
+    public void watch(){
+        System.out.println("Watch!");
+    }
+
+}
--- com.Itcast/PolymorphismB/src/Main.java
+++ com.Itcast/PolymorphismB/src/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         Animal animal = new Cat();
         animal.eat();
+
+        Cat cat = (Cat) animal;
+        cat.catchMouse();
+
+//        No Compiling Error, but will lead to a ClassCastException
+//        Dog dog = (Dog) animal;
+
+
     }
 }
