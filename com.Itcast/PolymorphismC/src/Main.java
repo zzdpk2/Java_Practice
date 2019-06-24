1b98b8c8770fab2f318d52a61bb90aeadbccc833
zhu00100 <zhu00100@umn.edu>
2019-06-24 01:54:10 -0700

    new

--- com.Itcast/PolymorphismC/src/Main.java
+++ com.Itcast/PolymorphismC/src/Main.java
@@ -10,9 +10,20 @@ public class Main {
         laptop.useDevice(usbMouse);
 
         Keyboard keyboard = new Keyboard();
-        laptop.useDevice(keyboard);
+        laptop.useDevice(keyboard); // cast to parent class
+        laptop.useDevice(new Keyboard()); //cast to a parent class via an anonymous class
 
+        method(10.0);  // double --> double
+        method(10); // int --> double
+
+        int a = 30;
+        method(a);  // int --> double
 
         laptop.powerOff();
     }
+
+    public static void method(double num){
+        System.out.println(num);
+    }
+
 }
