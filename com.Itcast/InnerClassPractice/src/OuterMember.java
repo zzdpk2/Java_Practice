64a11db5cf6ed1a8f0378933132fb70b47255d4f
zhu00100 <zhu00100@umn.edu>
2019-06-26 01:37:23 -0700

    new message

---
+++ com.Itcast/InnerClassPractice/src/OuterMember.java
@@ -0,0 +1,16 @@
+public class OuterMember {
+
+    public void methodOuter(){
+        class Inner{
+            int num = 10;
+            public void methodInner(){
+                System.out.println(num);
+            }
+        }
+
+        Inner inner = new Inner();
+        inner.methodInner();
+
+    }
+
+}
---
+++ com.Itcast/InnerClassPractice/src/OuterMemberEntrance.java
@@ -0,0 +1,6 @@
+public class OuterMemberEntrance {
+    public static void main(String[] args) {
+        OuterMember outerMember = new OuterMember();
+        outerMember.methodOuter();
+    }
+}
