7ca3f0d6982da2bcfb4e4423b14e80f36ff21b69
zhu00100 <zhu00100@umn.edu>
2019-06-26 23:10:53 -0700

    new

---
+++ com.Itcast/InnerClassPractice/src/ClassAsVars/Weapon.java
@@ -0,0 +1,21 @@
+package ClassAsVars;
+
+public class Weapon {
+
+    private String code;
+
+    public Weapon() {
+    }
+
+    public Weapon(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
