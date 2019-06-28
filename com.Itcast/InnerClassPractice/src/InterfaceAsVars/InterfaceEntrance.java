cce190869dd06d102341cc201657549ea621d414
zhu00100 <zhu00100@umn.edu>
2019-06-27 22:26:57 -0700

    new

--- com.Itcast/InnerClassPractice/src/InterfaceAsVars/InterfaceEntrance.java
+++ com.Itcast/InnerClassPractice/src/InterfaceAsVars/InterfaceEntrance.java
@@ -1,10 +1,23 @@
 package InterfaceAsVars;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class InterfaceEntrance {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
-        
+
+        List<String> result = addNames(list);
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println(result.get(i));
+        }
+    }
+
+    public static List<String> addNames(List<String> list){
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        list.add("Four");
+        return list;
     }
 }
