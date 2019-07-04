03af69fb04db8790e3e224a6b368f2070f157781
zzdpk2 <zzdpk2@126.com>
2019-07-04 10:40:57 -0700

    new

--- com.Itcast/PolyDef/src/Advance.java
+++ com.Itcast/PolyDef/src/Advance.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Advance {
     public static void main(String[] args) {
+//        show01();
+        show02();
+    }
 
+    private static void show02() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("abc");
+
+        Iterator<String> it = list.iterator();
+        while(it.hasNext())
+            System.out.println(it.next());
+
+    }
+
+    private static void show01() {
+        ArrayList list = new ArrayList();
+        list.add("ABC");
+        list.add(1);
+
+        Iterator it = list.iterator();
+        while(it.hasNext()){
+            Object obj = it.next();
+            System.out.println(obj);
+
+            // String 特有方法 需要向下转型 but ClassCastException
+//            String s = (String)obj;
+//            System.out.println(s.length());
+
+        }
     }
 }
