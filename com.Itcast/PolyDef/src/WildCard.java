d8f12691e1320b2d6f436b44a189121f8ce10c4b
zzdpk2 <zzdpk2@126.com>
2019-07-05 00:31:33 -0700

    new

--- com.Itcast/PolyDef/src/WildCard.java
+++ com.Itcast/PolyDef/src/WildCard.java
@@ -1,9 +1,21 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class WildCard {
     public static void main(String[] args) {
         ArrayList<Integer> list01 = new ArrayList<>();
         list01.add(1);
         list01.add(2);
+
+        ArrayList<String> list02 = new ArrayList<>();
+        list02.add("A");
+        list02.add("B");
     }
+
+    public static void printArray(ArrayList<?> list){
+        Iterator<?> it = list.iterator();
+        while(it.hasNext())
+            System.out.println(it.next());
+    }
+
 }
