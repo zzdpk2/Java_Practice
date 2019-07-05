63cc22643415b3f95b4d9e7b15e65daf497b6bf6
zzdpk2 <zzdpk2@126.com>
2019-07-04 23:17:56 -0700

    new

--- com.Itcast/PolyDef/src/GenericMethod.java
+++ com.Itcast/PolyDef/src/GenericMethod.java
@@ -2,4 +2,9 @@ public class GenericMethod {
     public <M> void method1(M m){
         System.out.println(m);
     }
+
+    public static <S> void method2(S s){
+        System.out.println(s);
+    }
+
 }
--- com.Itcast/PolyDef/src/GenericMethodEntrance.java
+++ com.Itcast/PolyDef/src/GenericMethodEntrance.java
@@ -1,3 +1,10 @@
 public class GenericMethodEntrance {
+    public static void main(String[] args) {
+        GenericMethod gm = new GenericMethod();
+        gm.method1(10);
+        gm.method1("abc");
+        gm.method1(true);
 
+        gm.method2("Static method!");
+    }
 }
