a34a4ec8616f7facd2fd6adb06d82ec6ab0632c2
zzdpk2 <zzdpk2@126.com>
2019-07-08 17:09:25 +0800

    new

---
+++ com.Itcast/SetUse/src/SetNotDup.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class SetNotDup {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        String s1 = "abc";
+        String s2 = "abc";
+        set.add(s1);
+        set.add(s2);
+        set.add("重地");
+        set.add("通话");
+        set.add("abc");
+
+        System.out.println(set);
+    }
+}
