7e81b9b4292355976e20e7f11684d7bcac18d78b
zzdpk2 <zzdpk2@126.com>
2019-07-15 15:09:05 +0800

    new

--- com.Itcast/throwable-use/src/exception/FinallyReturn.java
+++ com.Itcast/throwable-use/src/exception/FinallyReturn.java
@@ -1,4 +1,21 @@
 package exception;
 
 public class FinallyReturn {
+
+    public static void main(String[] args) {
+        System.out.println(getA());
+    }
+
+    public static int getA(){
+        int a = 10;
+        try{
+            return a;
+        }catch (Exception e){
+            System.out.println(e);
+        }finally {
+            a = 100;
+            return a;
+        }
+    }
+
 }
