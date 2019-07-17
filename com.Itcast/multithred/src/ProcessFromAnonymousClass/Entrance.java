c2d747d6126d36d140efda9c61431edadccde90d
zzdpk2 <zzdpk2@126.com>
2019-07-17 16:57:24 +0800

    new

---
+++ com.Itcast/multithred/src/ProcessFromAnonymousClass/Entrance.java
@@ -0,0 +1,31 @@
+package ProcessFromAnonymousClass;
+
+public class Entrance {
+    public static void main(String[] args) {
+        new Thread(){
+            @Override
+            public void run() {
+                for (int i = 0; i < 20; i++)
+                    System.out.println(Thread.currentThread().getName() + "-->" + " pc");
+            }
+        }.start();
+
+//        Runnable r = new Runnable(){
+//            @Override
+//            public void run() {
+//                for (int i = 0; i < 20; i++)
+//                    System.out.println(Thread.currentThread().getName() + "-->" + " pc2");
+//            }
+//        };
+//        new Thread(r).start();
+
+        // Simplify
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                for (int i = 0; i < 20; i++)
+                    System.out.println(Thread.currentThread().getName() + "-->" + " pc2");
+            }
+        }).start();
+    }
+}
