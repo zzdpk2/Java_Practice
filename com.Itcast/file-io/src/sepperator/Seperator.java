cd17ab2ed902784870037d9ecff437c9d299a4c1
zzdpk2 <zzdpk2@126.com>
2019-12-31 15:41:28 +0800

    new

---
+++ com.Itcast/file-io/src/sepperator/Seperator.java
@@ -0,0 +1,10 @@
+package sepperator;
+
+import java.io.File;
+
+public class Seperator {
+    public static void main(String[] args) {
+        String pathSeperator = File.separator;
+        System.out.println(pathSeperator);
+    }
+}
